843e52c8db837f4e096cf1b0c737457bffea9c05
dimax0505 <dimax0505@gmail.com>
2018-12-10 14:50:45 +0300

    немножко оптимизации

--- src/main/java/Lesson6/ArrayAfterFour.java
+++ src/main/java/Lesson6/ArrayAfterFour.java
@@ -4,21 +4,20 @@ import java.util.Arrays;
 
 public class ArrayAfterFour {
 
-   // private int[] arr;
-
-
-//    ArrayAfterFour(int[] arr) {
-//        this.arr = arr;
-//    }
-
-    public int[] getArrAfterFour(int[] arr) throws RuntimeException{
+    public int[] getArrAfterFour(int[] arr) throws RuntimeException {
         int index = -1;
 
-            for(int i = 0; i<arr.length; i++) {
-                if (arr[i]==4) index = i;
-            }
-            if (index==-1)throw new RuntimeException();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 4) index = i;
+        }
+        if (index == -1) throw new RuntimeException();
 
-       return Arrays.copyOfRange(arr,index+1,arr.length);
+        return Arrays.copyOfRange(arr, index + 1, arr.length);
+    }
+
+    public boolean getArrOneFour(int[] arr) {
+        for (int anArr : arr)
+            if (!(anArr == 4 || anArr == 1)) return false;
+        return true;
     }
 }
